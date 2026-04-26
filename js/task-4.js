4a89ae1d06bced3b94319c7869844f83d1ef3fd7
mykhailo-tyshynskyi <tihan.cs@gmail.com>
2026-04-26 19:07:55 +0300

    reworked

--- js/task-4.js
+++ js/task-4.js
@@ -5,14 +5,14 @@ function handleFormSubmit(e) {
   e.preventDefault();
   const elEmail = e.target.elements.email;
   const elPassword = e.target.elements.password;
-  if (elEmail.value === "" || elPassword.value === "") {
+  if (elEmail.value.trim() === "" || elPassword.value.trim() === "") {
     alert("All form fields must be filled in");
   } else {
     const inputs = form.querySelectorAll("input");
     const data = {};
 
     for (const input of inputs) {
-      data[input.name] = input.value;
+      data[input.name] = input.value.trim();
     }
     console.log(data);
     form.reset();
